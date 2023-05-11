08284212cc9b1ca663989a3a6bffe34e69b75b64
uac35 <uygarts9@gmail.com>
2023-05-11 13:45:00 +0200

    Heat Map with time sliders
    Labels for dates on the slider will be added later. See df_final.csv for the data used in this code. It visualizes the number of occurrences of singular events chosen from dropdown

--- MDA/src/visualization/timemap.py
+++ MDA/src/visualization/timemap.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import pandas as pd
 import numpy as np
 import sys
@@ -7,95 +7,85 @@ import folium
 from folium.plugins import HeatMap
 from folium.plugins import HeatMapWithTime
 import ipywidgets as widgets
-import tkinter as tk
-from tk_html_widgets import HTMLLabel
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QSlider
-from PyQt5.QtCore import Qt
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QSlider, QComboBox
 from PyQt5.QtCore import Qt, QUrl
-
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+#path for whereever df_final.csv is stored
 path = "C:\\Users\\uygar\\Desktop\\DataAnalytics\\export_40"
 os.chdir(path)
-# %%
+
+
+##Logging
+def redirect_print_to_logfile(logfile):
+    # Remove the existing logfile if it exists
+    if os.path.exists(logfile):
+        os.remove(logfile)
+    
+    # Open the logfile in append mode
+    sys.stdout = open(logfile, "a")
+# Set the logfile name
+logfile = "output.log"
+# Redirect print statements to the logfile
+redirect_print_to_logfile(logfile)
+
+
+
+##Heat Map
 class Location:
-    def __init__(self, identifier, lat, long):
-        self.identifier = identifier
+    def __init__(self, lat, long):
         self.lat = lat
         self.long = long
-        self.row = 1
-    def get_address(self):
-        parts = self.identifier.split("_")
-        if len(parts) < 5:
-            return "Invalid identifier"
-        street_name = parts[4].replace("-", " ").replace(".csv", "")
-        return street_name + ", Leuven, Belgium"
-    
-    def get_all_db_values(self):
-        filename = os.path.splitext(self.identifier)[0] + ".csv"
-        if not os.path.isfile(filename):
-            print(f"File does not exist: {filename}")  #debug and error check
-            return []
-        df = pd.read_csv(filename, sep=";", engine="python")
-        df['result_timestamp'] = pd.to_datetime(df['result_timestamp'], format='%d/%m/%Y %H:%M:%S.%f')
-        db_values = df["laf005_per_hour"]
-        time_values = df['result_timestamp']
-        #print(time_values.head())  # debug
-        #print(db_values.head())  #debug
-        return list(zip(db_values, time_values))
+        self.db_values = dict()
 
 
 
-# %%
-# Create Location objects for each location
-locations = [
-    Location("csv_results_40_255439_mp-01-naamsestraat-35-maxim.csv", 50.877160828051615, 4.7006992525313445),
-    Location("csv_results_40_255440_mp-02-naamsestraat-57-xior.csv", 50.87649057167669, 4.700691639839767),
-    Location("csv_results_40_255441_mp-03-naamsestraat-62-taste.csv", 50.875850541602986, 4.700192168675618),
-    Location("csv_results_40_303910_mp-04-his-hears.csv", 50.87525620749695, 4.700106135276407),
-    Location("csv_results_40_255442_mp-05-calvariekapel-ku-leuven.csv", 50.87448678827803, 4.699889625684066),
-    Location("csv_results_40_255443_mp-06-parkstraat-2-la-filosovia.csv", 50.87409073433792, 4.700018371714718),
-    Location("csv_results_40_255444_mp-07-naamsestraat-81.csv", 50.87381822269834, 4.70010170607578),
-    Location("csv_results_40_255445_mp-08-kiosk-stadspark.csv", 50.87526203165543, 4.701475911003796),
-    Location("csv_results_40_280324_mp08bis---vrijthof.csv", 50.87890579999999, 4.7011444846213415),
-]
+def load_df():
+    df = pd.read_csv("df_final.csv")
+    df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+    return df
 
+def get_locations(df, noise_type):
+    locations = []
+    for _, row in df.iterrows():
+        locations.append(Location(row['date'], row['latitude'], row['longitude'], row[noise_type]))
+    return locations
 
-# %%
 def init_map():
     map = folium.Map(location=[locations[0].lat, locations[0].long], tiles='CartoDB Positron', zoom_start=15, zoom_control=False)
     return map
 
-
-# %%
-
-
-# %%
-def update_map():
+def update_map(noise_type):
+    print(f"Updating map with noise type: {noise_type}") #debug see if function execs
 
     # Generate all data points
     all_data = {}
     for loc in locations:
-        db_values = loc.get_all_db_values()
-        for db_value, time_value in db_values:
-            if time_value not in all_data:
-                all_data[time_value] = []
-            all_data[time_value].append([loc.lat, loc.long, db_value])
-    #print(all_data.keys()) #debug
+        for date, noise_data in loc.db_values.items():
+            if date not in all_data:
+                all_data[date] = []
+            
+            data_value = loc.db_values[date][noise_type]
+            print(f"Data value: {data_value}")  # debug, data values
+            all_data[date].append([loc.lat, loc.long, data_value])
+
     # Convert the dictionary to a list sorted by time
-    heatmap_data_time = [all_data[time] for time in sorted(all_data.keys())]
+    heatmap_data_time = [all_data[date] for date in sorted(all_data.keys())]
+    #Bias for showing 0s as blue?
+    #heatmap_data_time = [[[lat, lon, value + 1] for [lat, lon, value] in date_data] for date_data in heatmap_data_time]
 
     global map
     map = init_map()
     # add HeatMapWithTime instead of regular HeatMap
-    heatmap_layer = HeatMapWithTime(heatmap_data_time, auto_play=True, max_opacity=0.8)
+    gradient = {0: 'blue', 0.22:'purple',0.33: 'yellow', 0.77: 'orange', 1: 'red'}
+    heatmap_layer = HeatMapWithTime(heatmap_data_time, auto_play=True, max_opacity=0.8, gradient=gradient,radius=30)
     heatmap_layer.add_to(map)
 
-    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".html")
+    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".html",dir=path)
     temp_file.close()
     map.save(temp_file.name)
 
     web_view.load(QUrl.fromLocalFile(os.path.abspath(temp_file.name)))
-
+    web_view.reload()
 
 def remove_temp_file():
     try:
@@ -103,12 +93,50 @@ def remove_temp_file():
     except:
         pass
 
-#This is to show the map in a separate window
+def on_noise_type_changed(noise_type):
+    print(f"Changing noise type to {noise_type}") #debug see if function execs
+    update_map(noise_type)
+ 
+# Load the dataframe
+df = load_df()
+# Create list of unique location tuples
+location_tuples = df[['latitude', 'longitude']].drop_duplicates().values
+
+# Create list of Location objects
+locations = [Location(lat, long) for lat, long in location_tuples]
+
+# Iterate over dataframe rows and update db_values for each location
+for index, row in df.iterrows():
+    # Find location in locations list
+    for location in locations:
+        if location.lat == row['latitude'] and location.long == row['longitude']:
+            # Add db_values to this location
+            location.db_values[row['date']] = {
+                'Human voice - Shouting': row['Human voice - Shouting'],
+                'Human voice - Singing': row['Human voice - Singing'],
+                'Music non-amplified': row['Music non-amplified'],
+                'Nature elements - Wind': row['Nature elements - Wind'],
+                'Transport road - Passenger car': row['Transport road - Passenger car'],
+                'Transport road - Siren': row['Transport road - Siren'],
+                'Unsupported': row['Unsupported']
+            }
+
+
+# Get the list of noise types
+noise_types = df.columns.drop(['date', 'latitude', 'longitude','Unnamed: 0', 'description'])
+
+# Initialize the application
 app = QApplication(sys.argv)
 window = QWidget()
 layout = QVBoxLayout()
 
+# Create a combo box for noise type selection
+combo_box = QComboBox()
+combo_box.addItems(noise_types)
+combo_box.currentTextChanged.connect(on_noise_type_changed)
+layout.addWidget(combo_box)
 
+# Initialize the web view
 web_view = QWebEngineView()
 web_view.loadFinished.connect(remove_temp_file)
 layout.addWidget(web_view)
@@ -116,10 +144,8 @@ layout.addWidget(web_view)
 window.setLayout(layout)
 window.show()
 
-#Update map with time info
-update_map()
+# Update map with the first noise type
+update_map(noise_types[0])
 
-sys.exit(app.exec_())
-
-
-
+# Run the application
+sys.exit(app.exec_())        
