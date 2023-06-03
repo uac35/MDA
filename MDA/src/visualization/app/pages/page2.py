ba645ee92bb4bd6f7ce6e7352e6874e44ec7d094
PjCnudde <82454477+PjCnudde@users.noreply.github.com>
2023-06-03 13:24:38 +0200

    Changed sliders to text input

--- MDA/src/visualization/app/pages/page2.py
+++ MDA/src/visualization/app/pages/page2.py
@@ -7,51 +7,16 @@ dash.register_page(__name__)
 
 layout = html.Div(
     [
-        dbc.Container(
-            [
-                dbc.Row(
-                    [
-                        dbc.Col(
-                            [dcc.Graph(id="map", style={"width": "100%", "padding-left": "20px"})],
-                            width=12,
-                        ),
-                    ]
-                ),
-                dbc.Row(
-                    [
-                        dbc.Col(
-                            [
-                                html.Label("Date"),
-                                dcc.DatePickerSingle(id="date-picker", date=datetime.now()),
-                            ],
-                            width=3,
-                        ),
-                        dbc.Col(
-                            [
-                                html.Label("Temperature"),
-                                dcc.Slider(
-                                    id="temperature-slider", min=0, max=50, step=0.5, value=25
-                                ),
-                            ],
-                            width=3,
-                        ),
-                        dbc.Col(
-                            [
-                                html.Label("Rainfall"),
-                                dcc.Slider(id="rainfall-slider", min=0, max=500, step=5, value=100),
-                            ],
-                            width=3,
-                        ),
-                        dbc.Col(
-                            [
-                                html.Label("Windspeed"),
-                                dcc.Slider(id="windspeed-slider", min=0, max=30, step=1, value=10),
-                            ],
-                            width=3,
-                        ),
-                    ]
-                ),
-            ]
-        )
+        html.H1("Predictive Model"),
+        html.Label("Date and Hour (format: YYYY-MM-DD HH):"),
+        dcc.Input(id="date-input", type="text"),
+        html.Label("LC_TEMP value:"),
+        dcc.Input(id="temp-input", type="number"),
+        html.Label("LC_RAIN value:"),
+        dcc.Input(id="rain-input", type="number"),
+        html.Label("LC_WIND value:"),
+        dcc.Input(id="wind-input", type="number"),
+        html.Button("Predict", id="predict-button", n_clicks=0),
+        html.Div(id="prediction-output"),
     ]
 )
